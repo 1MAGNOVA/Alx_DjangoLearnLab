5a1710ce90a6aaa2783ab06c14c20b203702c023
1MAGNOVA <olalekanabsm@gmail.com>
2025-02-18 18:04:12 -0800

    added

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
+from .models import Book
 from django.contrib import admin
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+   list_display = ("title" ,"author", "publication_year")
+   search_fields = ("title", "author" , "publication_year")
+
+
+admin.site.register(Book, BookAdmin)
