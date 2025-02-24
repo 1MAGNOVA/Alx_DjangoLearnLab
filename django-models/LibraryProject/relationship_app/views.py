c8945eb67862fd4cef3892a7ae2df4d9d02101f8
1MAGNOVA <olalekanabsm@gmail.com>
2025-02-23 17:16:44 -0800

    query added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
+from .models import Book
 from django.shortcuts import render
 
 # Create your views here.
+def ListView(request):
+    books = Books.objects.all()
+    context = {'list_books':books}
+    return render(request, "relationship_app/list_books.html", {"books":books} ) 
