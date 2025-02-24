4626e0aab5098758323c9875c903755bf1d3b7fa
1MAGNOVA <olalekanabsm@gmail.com>
2025-02-23 16:29:16 -0800

    query added

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,14 +9,14 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author= models.ForeignKey(Author, on_delete =models.CASCADE, related_names= "books")
+    author= models.ForeignKey(Author, on_delete =models.CASCADE, related_name= "books")
     
     def __str__(self):
         return self.title
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(book, related_name="books")
+    books = models.ManyToManyField(Book, related_name="books")
 
     def __str__(self):
         return self.name
---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,12 @@
+import os
+import django
+
+# Set up Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-models.settings")
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+author_name == (author_name) 
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(f"Books by {author_name}: {[book.title for book in books_by_author]}
