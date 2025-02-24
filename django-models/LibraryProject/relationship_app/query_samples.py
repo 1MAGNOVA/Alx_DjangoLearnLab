575fe6a20a62b3e1fd2580c4eddfbbf7bf5447fe
1MAGNOVA <olalekanabsm@gmail.com>
2025-02-23 16:50:24 -0800

    query added

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,4 +9,9 @@ from relationship_app.models import Author, Book, Library, Librarian
 author_name == (author_name) 
 author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
-print(f"Books by {author_name}: {[book.title for book in books_by_author]}
+print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
+
+# List all books in a library
+library_name == (library_name)
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
