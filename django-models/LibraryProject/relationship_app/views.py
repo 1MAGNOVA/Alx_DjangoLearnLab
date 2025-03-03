b80f2aeab330764f4228a101b3edb6c8802dc568
1MAGNOVA <olalekanabsm@gmail.com>
2025-03-03 15:04:45 -0800

    added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,12 +12,7 @@ from django.views.generic.detail import DetailView
 def ListView(request):
     books = Book.objects.all()
     context = {'list_books':books}
-    return render(request, "relationship_app/list_books.html", {"books":books} ) 
-
-def list_books(request):
-    books = Book.objects.all()
-    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(book_list, content_type="text/plain")
+    return render(request, "relationship_app/list_books.html", {"books":books} )
 
 #Create a class-based view 
 def BookDetailView(DetailView):
