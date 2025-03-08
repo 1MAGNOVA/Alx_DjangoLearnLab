ec0c6a6a458a43ef3fb602485ca44a8cfc349acd
1MAGNOVA <olalekanabsm@gmail.com>
2025-03-08 10:44:58 -0800

    login added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,8 @@ from .views import list_books
 urlpatterns = [
     path('list_books/', list_view, name='list_books'),
     path('library/', LibraryDetailView.as_view(), name='library_detail'),
-        ]
+    path('login/', loginView.as_view(template_name='registration/login.html'), name='login'),
+    ]
 
 
 
